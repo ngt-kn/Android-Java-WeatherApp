04bd1a197876141af9553936cfc59465600d06dc
ngt-kn <ngt.kn.dev@gmail.com>
2018-07-22 03:51:52 -0700

    add FragmentViewPagerAdapter

--- app/src/main/java/com/ngtkn/weatherappandroid/data/ForecastViewPagerAdapter.java
+++ app/src/main/java/com/ngtkn/weatherappandroid/data/ForecastViewPagerAdapter.java
@@ -1,6 +1,28 @@
 package com.ngtkn.weatherappandroid.data;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 
-public class ForecastViewPagerAdapter extends ViewPager {
+import java.util.List;
+
+public class ForecastViewPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragments;
+
+    public ForecastViewPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return this.fragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return this.fragments.size();
+    }
 }
